fe66c8f44f37504de52ac5365aae24e9e8d8935f
N'Deko Aline <aline.ndeko@etudiant.univ-rennes.fr>
2025-04-29 17:12:14 +0200

    step 1

--- pokedemo/src/app/my-component/my-component.component.ts
+++ pokedemo/src/app/my-component/my-component.component.ts
@@ -7,16 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './my-component.component.css'
 })
 export class MyComponentComponent {
-    id: string = '';
 
-    pokes: Pokemon[] = [];
-
-    constructor(){
-      this.pokes.push(new Pokemon('1' , 'Pikachou') );
-      this.pokes.push(new Pokemon('2' , 'bulbasaur') );
-      this.pokes.push(new Pokemon('3' , 'ivysaur') );
-      this.pokes.push(new Pokemon('4' , 'venusaur') );
-      this.pokes.push(new Pokemon('5' , 'charmander') );
-
-    }
 }
